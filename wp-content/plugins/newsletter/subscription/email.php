c5d892ace95f938bacaf39d9fdb22321c463c28a
Sebastian Otto <sotto@wemakeweb.de>
2014-06-29 15:27:39 +0200

    added email l&t header

--- wp-content/plugins/newsletter/subscription/email.php
+++ wp-content/plugins/newsletter/subscription/email.php
@@ -36,15 +36,12 @@ if (is_file(WP_CONTENT_DIR . '/extensions/newsletter/subscription/email.php')) {
             <tr>
                 <td style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-size: 14px; color: #666;">
                     <div style="text-align: left; max-width: 500px; border-top: 10px solid #43A4D0; border-bottom: 3px solid #43A4D0;">
-                        <div style="padding: 10px 20px; color: #000; font-size: 20px; background-color: #EFEFEF; border-bottom: 1px solid #ddd">
-                            <?php echo get_option('blogname'); ?>
-                        </div>
+                       <img src="http://lobundtadel.es/wp-content/themes/lob%26tadel/image/lt-email-header.png">
                         <div style="padding: 20px; background-color: #fff; line-height: 18px">
 
                             <?php echo $message; ?>
 
                         </div>
-
                     </div>
                 </td>
             </tr>
