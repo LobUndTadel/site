6131efaaeba641307d258ae30f51c3a17c6926b4
Sebastian Otto <sotto@wemakeweb.de>
2014-06-30 11:38:11 +0200

    fucking og:image

--- wp-content/themes/lob&tadel/front-page.php
+++ wp-content/themes/lob&tadel/front-page.php
@@ -13,7 +13,7 @@
         <meta property="og:title" content="Lob&Tadel">
         <meta property="og:type" content="website">
         <meta property="og:url" content="http://lobundtadel.es">
-        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/social.jpg">
+        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/social2.jpg">
         <meta property="og:site_name" content="lobundtadel.es">
         <meta property="fb:app_id" content="609003865836229">
         <meta property="fb:admins" content="100000211543747" />
